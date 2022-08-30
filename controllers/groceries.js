9328819cfd58e054d14a8faf01d153e7d314f577
MichaelLinch90 <michaellinch90@gmail.com>
2022-08-30 15:03:12 -0700

    add method override. 1st attempt to add delete method

--- controllers/groceries.js
+++ controllers/groceries.js
@@ -10,7 +10,7 @@
   
   function index(req, res) {
     Grocery.find({}, function (err, groceries) {
-        // if (err) return res.redirect('/');
+        if (err) return res.redirect('/');
         console.log(groceries)
         res.render('./groceries', { groceries })
         
@@ -21,9 +21,34 @@
     res.render('./groceries')
   }
 
+  // Include the next parameter - used for error handling in the catch
+function deleteGrocery(req, res, next) {
+    // Note the cool "dot" syntax to query on the property of a subdoc
+    Grocery.findOne({'groceries:_id' : req.params.id}).then(function(grocery) {
+        console.log(grocery)
+      // Find the review subdoc using the id method on Mongoose arrays
+      // https://mongoosejs.com/docs/subdocs.html
+      const item = groceries.id(req.params.id);
+      // Ensure that the review was created by the logged in user
+    //   if (!review.user.equals(req.user._id)) return res.redirect(`/movies/${movie._id}`);
+      // Remove the review using the remove method of the subdoc
+      item.remove();
+      // Save the updated movie
+      grocery.save().then(function() {
+        // Redirect back to the movie's show view
+        res.redirect(`/groceries`);
+      }).catch(function(err) {
+        // Let Express display an error
+        return next(err);
+        // res.redirect(`/movies/${movie._id}`);
+      });
+    });
+  }
+
   module.exports = {
     create,
     index,
-    new :newItem
+    new :newItem,
+    delete : deleteGrocery
   };
   
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 require("dotenv").config(); // for .env file
 
 // connect to the database with Mongoose
@@ -25,10 +26,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', banksRouter);
 app.use('/users', usersRouter);
 app.use('/groceries', groceriesRouter)
+app.use(function(req, res, next) {
+  console.log('Hello SEI!');
+  next();  // Pass the request to the next middleware
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
