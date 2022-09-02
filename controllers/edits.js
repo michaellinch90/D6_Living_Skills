4e4eb45840cf6847379be42abec4263cde6c8962
MichaelLinch90 <michaellinch90@gmail.com>
2022-09-02 08:06:52 -0700

    resolved ut method issue

--- controllers/edits.js
+++ controllers/edits.js
@@ -4,22 +4,13 @@ function editGrocery (req, res) {
     console.log('hello world')
     Grocery.findOne({_id: req.params.id}, function(err, grocery){
         if (err || !grocery) return res.redirect('/groceries');
-        //takes you to views/editGroceries
         res.render('./editGroceries', {title: "Edit grocery", grocery});
     });
-///:id/update
 }
 
 function update(req, res) {
-    console.log('i am callllllllled')
     Grocery.findOneAndUpdate(
-      {_id: req.params.id},
-      // update object with updated properties
-      req.body,
-      
-      // options object with new: true to make sure updated doc is returned
-      {new: true},
-     
+      {_id: req.params.id},req.body,{new: true},
       function(err, grocery) {
         if (err || !grocery) return res.redirect(`/`);
         res.redirect(`/groceries`);
--- routes/edits.js
+++ routes/edits.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const editCTRL = require('../controllers/edits');
 
-router.get('/editGroceries/:id', editCTRL.editGrocery)//link to edit page
+router.get('/:id/editGroceries', editCTRL.editGrocery)//link to edit page
+router.put('/:id/updateGroceries', editCTRL.update)// Update grocery
 
-router.put('/', editCTRL.update)// Update grocery
 
 module.exports = router ;
